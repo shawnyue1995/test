2e1cfd6fe3e7a4cd3a09510137c7a9a0572dd862
shawnyue1995 <67847027+shawnyue1995@users.noreply.github.com>
2020-07-07 14:00:42 +0800

    第三阶段

--- cgb-ajax-01/src/main/java/com/cy/controller/AjaxController.java
+++ cgb-ajax-01/src/main/java/com/cy/controller/AjaxController.java
@@ -4,12 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/ajax/")
 public class AjaxController {
+    static Map<String, Object> database = new HashMap<>();
+
+    static {
+        database.put("A", "100");
+        database.put("B", "200");
+
+    }
+
     @RequestMapping("doAjaxGetRequest")
     @ResponseBody/*将响应的内容以串的形式返回*/
-    public String doAjaxGetRequest() {
-        return "ajax get response result";
+    public String doAjaxGetRequest(String key) {
+        return "query data by key " + key;
     }
+
 }
