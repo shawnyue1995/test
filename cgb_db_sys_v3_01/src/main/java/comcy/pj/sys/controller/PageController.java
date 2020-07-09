4f14ec8cbb5d88920d305fc2c2d03e5beb5da3b3
shawnyue1995 <67847027+shawnyue1995@users.noreply.github.com>
2020-07-09 14:03:19 +0800

    第三阶段

---
+++ cgb_db_sys_v3_01/src/main/java/comcy/pj/sys/controller/PageController.java
@@ -0,0 +1,13 @@
+package comcy.pj.sys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
+@Controller
+public class PageController {
+    @RequestMapping("doIndexUI")
+    public String doIndexUI() {
+        return "starter";
+    }
+}
